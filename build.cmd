5853117a214b93b6c54c42734362a563e2ff6575
Ken Dale <ken@kendaleiv.com>
2017-03-02 15:27:59 -0500

    Use yarn if available

--- build.cmd
+++ build.cmd
@@ -3,8 +3,14 @@ pushd %~dp0
 setlocal
 
 :Build
-call npm install
-if %ERRORLEVEL% neq 0 goto BuildFail
+
+:: https://stackoverflow.com/a/27014081 answering https://stackoverflow.com/questions/10686737/check-whether-command-is-available-in-batch-file/27014081#27014081
+call where yarn >nul 2>nul
+if %ERRORLEVEL% == 1 (
+  call npm install
+) else (
+  call yarn install
+)
 
 call npm run validate
 if %ERRORLEVEL% neq 0 goto BuildFail
